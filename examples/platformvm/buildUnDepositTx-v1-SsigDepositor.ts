329a4d1ef10edbb6efbca3f9faf8f96b8e87aa78
Mohamed Elmoslemany <mohamed.elmoslemany@chain4travel.com>
2025-02-12 22:52:53 +0100

    update undeposit example

--- examples/platformvm/buildUnDepositTx-v1-SsigDepositor.ts
+++ examples/platformvm/buildUnDepositTx-v1-SsigDepositor.ts
@@ -1,4 +1,4 @@
-import { Avalanche, BinTools, Buffer } from "caminojs/index"
+import { Avalanche, Buffer } from "caminojs/index"
 import {
   PlatformVMAPI,
   KeyChain,
@@ -7,12 +7,9 @@ import {
   UTXOSet,
   GetUTXOsResponse
 } from "caminojs/apis/platformvm"
-import { OutputOwners } from "caminojs/common/output"
-import { PrivateKeyPrefix, DefaultLocalGenesisPrivateKey } from "caminojs/utils"
 import { ExamplesConfig } from "../common/examplesConfig"
 import BN from "bn.js"
-import createHash from "create-hash"
-import { SignerKeyPair } from "caminojs/common"
+
 
 //const config: ExamplesConfig = require("../common/examplesConfig.json")
 const avalanche: Avalanche = new Avalanche(
@@ -21,11 +18,9 @@ const avalanche: Avalanche = new Avalanche(
   "https",
   1002
 )
-const ownerPrivKey: string =
-  "PrivateKey-XXXX"
 
-const bintools: BinTools = BinTools.getInstance()
-const privKey: string = "PrivateKey-XXX"
+
+const privKey: string = "PrivateKey-XXXX"
 let pchain: PlatformVMAPI
 let pKeychain: KeyChain
 let pAddresses: Buffer[]
@@ -42,34 +37,12 @@ const InitAvalanche = async () => {
 
 const main = async (): Promise<any> => {
   await InitAvalanche()
-  const depositOwner = "P-kopernikus1d0ydd4wyq670lzrp0y7xd3wej4z7ysp2surum8"
   const amount_cam = 0.3
   const amountToUnLock = new BN(amount_cam*1000000000)
-  const depositOfferID = "BKLeCFQtxR9HKjBE3QbZtqjdWpWc59B2JwVf8pC37vneiEvJY"
-  const depositDuration = 10 * 24 * 3600
-  const depositTxID = "xP7nExUDq4STJodLE5sznnpprKEWrhmEKwTjFgLZJdmSN3jrV"
   const memo: Buffer = Buffer.from(
     "unDepositTx with singlesig deposit "
   )
 
-  const depositCreator = pAddresses[0]
-  const rewardsOwner = new OutputOwners([depositCreator], undefined, 1)
-  const depositCreatorAuth: [number, string | Buffer][] = [[0, depositCreator]]
-
-  const depositOfferOwnerAuth: [number, string | Buffer][] = [[0, depositOwner]]
-
-  // hash concatenated bytes of offer id and deposit owner address
-  const msgHashed: Buffer = Buffer.from(
-    createHash("sha256")
-      .update(
-        Buffer.concat([bintools.cb58Decode(depositOfferID), depositCreator])
-      )
-      .digest()
-  )
-
-  const keypair: SignerKeyPair = pKeychain.importKey(ownerPrivKey)
-  // sign the hash
-  const signatureBuffer: Buffer = keypair.sign(msgHashed)
 
   let utxoStrings:  string[] = [""]       
   let platformvmUTXOResponse: GetUTXOsResponse
@@ -86,12 +59,8 @@ const main = async (): Promise<any> => {
     }
     delay(100)
   }
-  let utxo = utxoSet.getUTXO("2DWBL7TXUfSKnGRzX8CbHVDCUJY9HMAYsiYBGwzv3NmuHrnhMo")
-  let utxoSet2: UTXOSet = new UTXOSet()
-  utxoSet2.add(utxo)
 
   utxoStrings = utxoSet.getAllUTXOStrings()
-  console.log(`UTXos: ${utxoStrings}`)
 
   const unsignedTx: UnsignedTx = await pchain.buildUnlockDepositTx(
     utxoSet,
