cfb12f593fdd8f521fd657d4326f50534b0841dd
ev.lekht <ev.lekht@gmail.com>
2025-04-25 15:12:29 +0400

    cleanup

--- src/apis/platformvm/api.ts
+++ src/apis/platformvm/api.ts
@@ -732,10 +732,10 @@ export class PlatformVMAPI extends JRPCAPI {
         return {
           rewardOwner: c.rewardOwner
             ? ({
-              locktime: new BN(c.rewardOwner.locktime),
-              threshold: new BN(c.rewardOwner.threshold).toNumber(),
-              addresses: c.rewardOwner.addresses
-            } as Owner)
+                locktime: new BN(c.rewardOwner.locktime),
+                threshold: new BN(c.rewardOwner.threshold).toNumber(),
+                addresses: c.rewardOwner.addresses
+              } as Owner)
             : undefined,
           validatorRewards: new BN(c.validatorRewards),
           expiredDepositRewards: new BN(c.expiredDepositRewards)
@@ -1698,7 +1698,7 @@ export class PlatformVMAPI extends JRPCAPI {
     } else if (!(sourceChain instanceof Buffer)) {
       throw new ChainIdError(
         "Error - PlatformVMAPI.buildImportTx: Invalid destinationChain type: " +
-        typeof sourceChain
+          typeof sourceChain
       )
     }
     const atomicUTXOs: UTXOSet = await (
@@ -1791,7 +1791,7 @@ export class PlatformVMAPI extends JRPCAPI {
     } else if (!(destinationChain instanceof Buffer)) {
       throw new ChainIdError(
         "Error - PlatformVMAPI.buildExportTx: Invalid destinationChain type: " +
-        typeof destinationChain
+          typeof destinationChain
       )
     }
     if (destinationChain.length !== 32) {
@@ -1990,7 +1990,7 @@ export class PlatformVMAPI extends JRPCAPI {
     if (stakeAmount.lt(minStake)) {
       throw new StakeError(
         "PlatformVMAPI.buildAddDelegatorTx -- stake amount must be at least " +
-        minStake.toString(10)
+          minStake.toString(10)
       )
     }
 
@@ -2105,7 +2105,7 @@ export class PlatformVMAPI extends JRPCAPI {
     if (stakeAmount.lt(minStake)) {
       throw new StakeError(
         `PlatformVMAPI.${caller} -- stake amount must be at least ` +
-        minStake.toString(10)
+          minStake.toString(10)
       )
     }
 
@@ -2371,7 +2371,7 @@ export class PlatformVMAPI extends JRPCAPI {
     if (stakeAmount.lt(minStake)) {
       throw new StakeError(
         `PlatformVMAPI.${caller} -- stake amount must be at least ` +
-        minStake.toString(10)
+          minStake.toString(10)
       )
     }
 
@@ -3157,10 +3157,10 @@ export class PlatformVMAPI extends JRPCAPI {
       to:
         to.length > 0
           ? {
-            locktime: toLockTime.toString(10),
-            threshold: toThreshold,
-            addresses: to
-          }
+              locktime: toLockTime.toString(10),
+              threshold: toThreshold,
+              addresses: to
+            }
           : undefined,
       change:
         change.length > 0
